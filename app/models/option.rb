bda73861c90d26c54ff323b493f0bd6b23629317
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-13 13:22:49 +0100

    created option model schema table & rake db:migrate

---
+++ app/models/option.rb
@@ -0,0 +1,3 @@
+class Option < ActiveRecord::Base
+  attr_accessible :content, :question_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140513121650) do
+ActiveRecord::Schema.define(:version => 20140513122134) do
 
   create_table "jobs", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(:version => 20140513121650) do
     t.integer  "trade_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "options", :force => true do |t|
+    t.integer  "question_id"
+    t.text     "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "questions", :force => true do |t|
