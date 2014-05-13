540f51a525b531d2470fe0aa85b8d527dd8b15af
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-13 13:06:11 +0100

    updated trade model with job_ids and displayed listed jobs under trades in views file

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ActiveRecord::Base
-  attr_accessible :name, :jobs_trades_attributes
+  attr_accessible :name
 
-  has_many :trades, through: :jobs_trades
   has_many :jobs_trades
+  has_many :trades, through: :jobs_trades
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,6 +1,10 @@
 class Trade < ActiveRecord::Base
-  attr_accessible :name, :jobs_trades_attributes
+  attr_accessible :name, :jobs_trade_attributes, :job_ids
 
-  has_many :jobs, through: :jobs_trades
   has_many :jobs_trades
+  has_many :jobs, through: :jobs_trades
+  accepts_nested_attributes_for :jobs_trades, :reject_if => lambda { |a| a[:name].blank?}, :allow_destroy => true
+
+  validates :name, presence: true
+
 end
