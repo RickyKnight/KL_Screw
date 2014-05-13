298fb4d87d48eab961ac98f3dcaf9313288add2c
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-13 10:19:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Screw::Application.config.secret_token = '5e31b75dc01d806a23921f3be33041b6f011c28e88d08ecb5809d53448ba76687c410d9b4b0153cd66f5f8dc4f5ba5c2c65cc10d3b1f046f5f0de28c9fea7ca2'
