f72cde539e1e7e7ab8fa2c84ac0f9e64b1a997ac
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-15 12:30:08 +0100

    updated associations between user and question model & created role? method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :questions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +8,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :role
+
+  def role?(role_to_compare)
+    role.to_s == role_to_compare.to_s
+  end
 end
