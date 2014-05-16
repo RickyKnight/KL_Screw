92763954bd14c9312e580ceb43ccbd96ca29476c
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-16 11:36:38 +0100

    deleted table answers from schema via migration drop

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140516101535) do
-
-  create_table "answers", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20140516102756) do
 
   create_table "jobs", :force => true do |t|
     t.string   "name"
