d601d50ef1e3c538618af1aec12b687d6de7b4b7
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-05-16 11:09:57 +0100

    deleted assoiciations between user, jobs and trades. Updated home/index.html.erb with filter options and deleted users/home.html.erb file

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ActiveRecord::Base
   attr_accessible :content, :job_id, :question_type, :options_attributes
 
   belongs_to :job
-  belongs_to :user
   has_many :options, :dependent => :destroy
   accepts_nested_attributes_for :options, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
 end
